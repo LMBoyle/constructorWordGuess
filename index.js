9d1541d235c6f37dcdf33dd5446c78b319af2f7d
LMBoyle <boylelukem@gmail.com>
2019-09-14 17:20:39 -0400

    finished functions to console log and run through functions based on answers and added function if input is blank

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Word = require("./word.js");
+var colors = require("colors");
 var inquirer = require("inquirer");
 
 var wordOpt = ["argument", "boolean", "branch", "bracket", "callback", "constructor", "debug", "developer", "error", "expression", "foreach", "function", "github", "glitch", "javascript", "operator", "program", "statement", "terminal"];
@@ -14,6 +15,7 @@ var guessedLetters = [];
 
 // Pick a new word to guess
 function newWord() {
+  guesses = 10
   wordToGuess = wordOpt[Math.floor(Math.random() * wordOpt.length)];
   runWord = new Word(wordToGuess);
   for (var l = 0; l < runWord.lettersArr.length; l++) {
@@ -35,60 +37,100 @@ function pushBoo() {
 // Show the blanks and guessed letters on the screen
 function displayWord() {
   display = runWord.createString();
-  console.log(display)
+  console.log("\n" + display + "\n")
   prompt();
 };
 
 // Prompt the user to guess a letter
 function prompt(){
   if (guesses > 0 && booArr.includes(false)) {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "guess",
-        message: "Guess a letter!"
-      }
-    ]).then(function (ans) {
-      console.log(wordToGuess);
-      if (guessedLetters.includes(ans.guess)) {
-        console.log("Already guessed, try again");
-        prompt()
-      }
-      else {
-        guessedLetters.push(ans.guess.toLowerCase());
-        runWord.guessMade(ans.guess.toLowerCase());
-        letterGuessed = ans.guess.toLowerCase();
-        nextStep();
-      }
-    });
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "guess",
+          message: "Guess a letter!"
+        }
+      ]).then(function (ans) {
+        if (ans.guess === "") {
+          inquirer
+            .prompt([
+              {
+                type: "confirm",
+                name: "noAns",
+                message: "Do you want to keep playing?",
+                default: true
+              }
+            ]).then(function(ans){
+              if (ans.noAns) {
+                prompt()
+              }
+              else {
+                console.log("\nThanks for playing")
+                console.log("\n==================================\n")
+              }
+            });
+        }
+        else if (guessedLetters.includes(ans.guess)) {
+          console.log("\nAlready guessed, try again\n");
+          prompt();
+        }
+        else {
+          guessedLetters.push(ans.guess.toLowerCase());
+          runWord.guessMade(ans.guess.toLowerCase());
+          letterGuessed = ans.guess.toLowerCase();
+          nextStep();
+        }
+      });
   }
   else if (guesses === 0) {
-    console.log("Ran out of guesses!")
-    // TODO playAgain();
+    console.log("Ran out of guesses!\n");
+    console.log("The correct word was: " + wordToGuess + "\n");
+    playAgain();
   }
   else if (!booArr.includes(false)) {
-    console.log("You got it!")
-     //TODO playAgain();
+    console.log("You guessed right!!!\n")
+    playAgain();
   }
   else {
-    console.log("End")
+    console.log("Thanks for playing");
+    console.log("\n==================================\n")
   }
 }
 
 function nextStep() {
   // If letter exists, console log correct
   if (compareArr.includes(letterGuessed)) {
-    console.log("CORRECT!")
+    console.log("\nCORRECT!!!".green)
   }
   // If letter does not exist, console log incorrect
   // subract from guesses left
   else {
     guesses--;
-    console.log("INCORRECT! \nGuesses Left: " + guesses)
+    console.log("\nINCORRECT!!!".red)
+    console.log("\nGuesses Left: " + guesses);
   }
-  // TODO If all the letters are guessed, end game
-  // TODO Else, keep playing
   pushBoo();
+}
+
+function playAgain() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "playAgain",
+        message: "Would you like to play again?",
+        default: true
+      }
+    ]).then(function(ans){
+      if (ans.playAgain) {
+        newWord();
+      }
+      else {
+        console.log("\nThanks for playing")
+        console.log("\n==================================\n")
+      }
+    });
 }
 
 newWord();
